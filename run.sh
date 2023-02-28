228eab877a22fcac54f249914cc11ff27f173172
Pavel Kuzmin <Virus191288@gmail.com>
2023-02-28 21:17:21 +0500

    fix

--- run.sh
+++ run.sh
@@ -12,26 +12,13 @@ while [[ "$#" -gt 0 ]]; do
 done
 echo "auto db reload: $RELOAD"
 
-OPEN="no"
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-      -o|--open|-O|--OPEN) OPEN="yes"; shift ;;
-      *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
-done
-echo "auto browser open: $OPEN"
-
 if [[ "yes" == $RELOAD ]]
 then
   bundle exec rake db:create
   bundle exec rake db:reset
 fi
 
-if [[ "yes" == $OPEN ]]
-then
-  open http://127.0.0.1:3000/
-  open http://127.0.0.1:3000/widget_tests
-fi
+open http://127.0.0.1:3000/
+open http://127.0.0.1:3000/widget_tests
 
 foreman start -f Procfile.dev
