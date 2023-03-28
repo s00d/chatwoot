8a3f177721130029f3757f096f7251d35c33410d
Pavel Kuzmin <Virus191288@gmail.com>
2023-03-28 19:42:38 +0500

    fix

---
+++ public/packs/js/48-8d1d414dbf7592220ab9.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[48],{3049:function(e,n,t){"use strict";t.r(n);var o=t(1),l=Object(o.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("Component to view details of portal")])}),[],!1,null,null,null);n.default=l.exports}}]);
+//# sourceMappingURL=48-8d1d414dbf7592220ab9.chunk.js.map
