8c9b517f18823e1b22adf6e84cdea20a475c9bb7
Pavel Kuzmin <Virus191288@gmail.com>
2023-04-03 23:11:01 +0500

    add post

--- public/lite_sdk.js
+++ public/lite_sdk.js
@@ -1,4 +1,4 @@
-(function init() {
+(function init(pos = 'right') {
   var styles = `
 .woot-widget-bubble {
     background: #1f93ff;
@@ -63,7 +63,9 @@
   // eslint-disable-next-line vars-on-top
   var newDiv = document.createElement('div');
   newDiv.innerHTML =
-    '<button class="woot-widget-bubble woot-elements--right" style="background: rgb(33, 150, 243);">' +
+    '<button class="woot-widget-bubble woot-elements--' +
+    pos +
+    '" style="background: rgb(33, 150, 243);">' +
     '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAUVBMVEUAAAD///////////////////////////////////////////////////////////////////////////////////////////////////////8IN+deAAAAGnRSTlMAAwgJEBk0TVheY2R5eo+ut8jb5OXs8fX2+cjRDTIAAADsSURBVHgBldZbkoMgFIThRgQv8SKKgGf/C51UnJqaRI30/9zfe+NQUQ3TvG7bOk9DVeCmshmj/CuOTYnrdBfkUOg0zlOtl9OWVuEk4+QyZ3DIevmSt/ioTvK1VH/s5bY3YdM9SBZ/mUUyWgx+U06ycgp7D8msxSvtc4HXL9BLdj2elSEfhBJAI0QNgJEBI1BEBsQClVBVGDgwYOLAhJkDM1YOrNg4sLFAsLJgZsHEgoEFFQt0JAFGFjQsKAMJ0LFAexKgZYFyJIDxJIBNJEDNAtSJBLCeBDCOBFAPzwFA94ED+zmhwDO9358r8ANtIsMXi7qVAwAAAABJRU5ErkJggg==" alt="bubble-icon">' +
     '</button>';
 
