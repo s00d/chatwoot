30f9c892c508b4fd1acb081a508b0425d67be65c
Pavel Kuzmin <Virus191288@gmail.com>
2023-08-18 10:57:14 +0500

    fix user auth

--- app/controllers/api/v1/widget/contacts_controller.rb
+++ app/controllers/api/v1/widget/contacts_controller.rb
@@ -34,16 +34,16 @@ class Api::V1::Widget::ContactsController < Api::V1::Widget::BaseController
   private
 
   def identify_contact(contact)
-    contact_identify_action = ContactIdentifyAction.new(
-      contact: contact,
-      params: permitted_params.to_h.deep_symbolize_keys,
-      discard_invalid_attrs: true
-    )
     begin
+      contact_identify_action = ContactIdentifyAction.new(
+        contact: contact,
+        params: permitted_params.to_h.deep_symbolize_keys,
+        discard_invalid_attrs: true
+      )
       @contact = contact_identify_action.perform
     rescue ActiveRecord::RecordInvalid => e
-      print e
-      print e.message
+      Rails.logger.debug e
+      Rails.logger.debug e.message
       if e.record.is_a?(ActiveModel::Validations)
         raise unless e.record.errors.details[:email].any? { |error| error[:error] == :taken }
 
