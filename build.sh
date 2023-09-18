8357a4d9299beb3186e2c90403af8ef63ad34743
Pavel Kuzmin <Virus191288@gmail.com>
2023-09-18 11:15:14 +0500

    rebuild

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 rm -rf public/packs
-rake assets:precompile RAILS_ENV=production
+rake assets:clean assets:clobber assets:precompile RAILS_ENV=production NODE_OPTIONS=--openssl-legacy-provider
 git add public/*
