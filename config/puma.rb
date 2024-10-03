951683837c27e11a8fac8254d6d76955fd14243a
Pavel Kuzmin <Virus191288@gmail.com>
2024-10-03 18:26:04 +0500

    close puma port

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
 
-host '127.0.0.1'
+# host '127.0.0.1'
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port ENV.fetch('PORT', 3000)
